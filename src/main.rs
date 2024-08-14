634542319cc724f65889752845205c378e8b063d
Mahdi Ali-Raihan <asder8215@gmail.com>
2024-08-14 05:12:04 -0400

    Implemented trash and labels command appropriately; need to do some checks on results later; added comments around code; learned a bit more about borrowing and ownership policies

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
 extern crate google_gmail1 as gmail1;
-extern crate getopts;
 
-use getopts::Options;
 use gmail1::hyper::client::HttpConnector;
 use gmail1::hyper_rustls::HttpsConnector;
 use gmail1::Error;
 use gmail1::{Gmail, oauth2, hyper, hyper_rustls};
-use std::collections::{HashMap, HashSet};
-use std::fs;
-use std::process;
+use std::collections::{BTreeMap, HashMap, HashSet};
+use std::fs; // for debugging purposes 
 use clap::Parser;
 
 
 /// Attempts to authenticate and connect to user's email; returns the connected client on success
 /// Need to create a service account on Google Cloud Platform Console and put the client id in a client_secret.json
-/// You can follow this for more info: https://support.google.com/cloud/answer/6158849?hl=en#:~:text=Go%20to%20the%20Google%20Cloud%20Platform%20Console%20Credentials%20page.,to%20add%20a%20new%20secret.
-/// Much of this code inspired from: https://docs.rs/google-gmail1/latest/google_gmail1/index.html
+/// 
+/// You can follow this for more info: [Google Cloud Help](https://support.google.com/cloud/answer/6158849?hl=en#:~:text=Go%20to%20the%20Google%20Cloud%20Platform%20Console%20Credentials%20page.,to%20add%20a%20new%20secret.)
+/// 
+/// Much of this code inspired from: [Google Gmail1 Doc](https://docs.rs/google-gmail1/latest/google_gmail1/index.html)
 async fn create_client() -> Gmail<HttpsConnector<HttpConnector>> {
     // Get an ApplicationSecret instance by some means. It contains the `client_id` and
     // `client_secret`, among other things.
@@ -46,11 +45,11 @@ async fn create_client() -> Gmail<HttpsConnector<HttpConnector>> {
 
     // Test connection to see if user is authenticated and info can be retrieved
     let result = hub
-                      .users()
-                      .get_profile("me")
-                      .add_scope("https://mail.google.com/")
-                      .doit()
-                      .await;
+        .users()
+        .get_profile("me")
+        .add_scope("https://mail.google.com/")
+        .doit()
+        .await;
 
 
     // Displays whether the result indicates a successful connection or a failed one
@@ -78,10 +77,45 @@ async fn create_client() -> Gmail<HttpsConnector<HttpConnector>> {
     hub
 }
 
-/// Return a HashSet of all email message id
-async fn list_messages() -> HashSet<String> {
-    let hub = create_client().await;
+/// Fetches message from authenticated user's email given a message id
+/// Returns None if the message is nonexistent
+async fn get_messages(hub: &Gmail<HttpsConnector<HttpConnector>>, msg_ids: &String) -> Option<gmail1::api::Message>{
+    let mut result = hub
+        .users()
+        .messages_get("me", msg_ids)
+        .add_scope("https://mail.google.com/")
+        .doit()
+        .await;
 
+    let mut message;
+    match result {
+        Err(e) => match e {
+            // The Error enum provides details about what exactly happened.
+            // You can also just use its `Debug`, `Display` or `Error` traits
+                Error::HttpError(_)
+            |Error::Io(_)
+            |Error::MissingAPIKey
+            |Error::MissingToken(_)
+            |Error::Cancelled
+            |Error::UploadSizeLimitExceeded(_, _)
+            |Error::Failure(_)
+            |Error::BadRequest(_)
+            |Error::FieldClash(_)
+            |Error::JsonDecodeError(_, _) => {
+                println!("{}", e); 
+                message = None;
+            },
+        },
+        Ok(res) => {
+            let (result, msg) = res;
+            message = Some(msg);
+        },
+    };
+    message
+}
+
+/// Return a HashSet of all email message ids
+async fn list_messages(hub: &Gmail<HttpsConnector<HttpConnector>>) -> HashSet<String> {
     let (result, messages) = hub
         .users()
         .messages_list("me")
@@ -93,10 +127,10 @@ async fn list_messages() -> HashSet<String> {
     // TODO: Find a way to error check on result 
     // assert_eq!(result.status(), StatusCode::);
 
-    let messages = messages.messages.unwrap();
+    // let messages = messages.messages.unwrap();
     let mut message_set = HashSet::<String>::default();
 
-    for msg in messages {
+    for msg in messages.messages.as_ref().unwrap() {
         message_set.insert(msg.id.clone().unwrap());
     }
 
@@ -128,20 +162,18 @@ async fn list_messages() -> HashSet<String> {
 }
 
 /// Return a HashSet of all email message id by label id
-async fn list_messages_by_label(label_id: &str) -> HashSet<String> {
-    let hub = create_client().await;
-
+async fn list_messages_by_label(hub: &Gmail<HttpsConnector<HttpConnector>>, label_id: &str) -> HashSet<String> {
     let mut message_set = HashSet::<String>::default();
     let mut fetch_emails = true;
 
     let (mut result, mut messages) = hub
-            .users()
-            .messages_list("me")
-            .add_label_ids(label_id)
-            .add_scope("https://mail.google.com/")
-            .doit()
-            .await
-            .unwrap();
+        .users()
+        .messages_list("me")
+        .add_label_ids(label_id)
+        .add_scope("https://mail.google.com/")
+        .doit()
+        .await
+        .unwrap();
     while fetch_emails == true {
 
         // TODO: Find a way to error check on result 
@@ -149,48 +181,48 @@ async fn list_messages_by_label(label_id: &str) -> HashSet<String> {
 
         // println!("{:?}", messages);
 
-        let page_token = messages.next_page_token.clone();
+        // let page_token = messages.next_page_token.clone();
         // println!("{:?}", &page_token.unwrap());
 
-        let msgs = messages.messages.clone().unwrap();
+        // let msgs = messages.messages;
 
-        for msg in msgs {
+        for msg in messages.messages.as_ref().unwrap() {
             message_set.insert(msg.id.clone().unwrap());
         }
         
-        if page_token == None {
-            fetch_emails = false;
+        if let Some(page_token) = &messages.next_page_token {
+            //
+            (result, messages) = hub
+                .users()
+                .messages_list("me")
+                .page_token(page_token)
+                .add_label_ids(label_id)
+                .add_scope("https://mail.google.com/")
+                .doit()
+                .await
+                .unwrap();
         }
         else{
-            (result, messages) = hub
-                    .users()
-                    .messages_list("me")
-                    .page_token(&page_token.unwrap())
-                    .add_label_ids(label_id)
-                    .add_scope("https://mail.google.com/")
-                    .doit()
-                    .await
-                    .unwrap();
+            fetch_emails = false;
         }
     }
 
     message_set
 }
 
-/// Return a HashMap of label names and ids within user's email
-async fn list_labels() -> HashMap<String, String> {
-    let hub = create_client().await;
-
+/// Return a BTreeMap of label names and ids within user's email
+async fn list_labels(hub: &Gmail<HttpsConnector<HttpConnector>>) -> BTreeMap<String, String> {
     let (result, labels_list) = hub
-            .users()
-            .labels_list("me")
-            .doit()
-            .await
-            .unwrap();
-    let labels = labels_list.labels.unwrap();
+        .users()
+        .labels_list("me")
+        .doit()
+        .await
+        .unwrap();
+    // let labels = labels_list.labels.unwrap();
     // println!("{:?}", &labels);
-    let mut label_map = HashMap::<String, String>::default();
-    for label in labels {
+    // let mut label_map = HashMap::<String, String>::default();
+    let mut label_map = BTreeMap::<String, String>::default();
+    for label in labels_list.labels.as_ref().unwrap() {
         label_map.insert(label.name.clone().unwrap(), label.id.clone().unwrap());
         // println!("{:?}", &label.name.unwrap());
         // println!("{:?}", &label.id.unwrap());
@@ -199,39 +231,54 @@ async fn list_labels() -> HashMap<String, String> {
     label_map
 }
 
-async fn get_label_id(label_name: String) -> Option<String>{
+/// Retrieves label id given label name
+async fn get_label_id(hub: &Gmail<HttpsConnector<HttpConnector>>, label_name: &String) -> Option<String>{
     // println!("{:?}", &label_name);
-    let label_id = list_labels().await.get(&label_name).cloned();
+    let label_id = list_labels(hub).await.get(label_name).cloned();
     
     label_id
 }
 
-async fn trash_messages_from_label(label_name: String){
-    let hub = create_client().await;
+/// Trashes all emails from given labels
+async fn trash_messages_from_labels(hub: &Gmail<HttpsConnector<HttpConnector>>, label_names: Vec<String>){
+    for label in label_names {
+        let label_id = get_label_id(hub, &label).await.clone();
 
-    let label_id = get_label_id(label_name).await.clone();
+        if label_id == None {
+            println!("{} is a nonexistent label name", label);
+            continue;
+        }
 
-    if label_id == None {
-        println!("Nonexistent label name");
-        process::exit(1);
+        let list_of_msgs = list_messages_by_label(hub, &label_id.unwrap()).await;
+
+        for msg_id in list_of_msgs {
+                let (result, trash_msg) = hub
+                        .users()
+                        .messages_trash("me", &msg_id)
+                        .doit()
+                        .await
+                        .unwrap();
+        }
     }
-
-    let list_of_msgs = list_messages_by_label(&label_id.unwrap()).await;
-
-    for msg_id in list_of_msgs {
-            let (result, trash_msg) = hub
-                    .users()
-                    .messages_trash("me", &msg_id)
-                    .doit()
-                    .await
-                    .unwrap();
-    }
-
 }
 
-fn print_usage(program: &str, opts: Options) {
-    let brief = format!("Usage: {} FILE [options]", program);
-    print!("{}", opts.usage(&brief));
+/// Trash emails from provided message ids
+async fn trash_messages_from_id(hub: &Gmail<HttpsConnector<HttpConnector>>, msg_ids: Vec<String>){
+    for msg_id in msg_ids {
+
+        if let Some(msg) = get_messages(hub, &msg_id).await {
+            let (result, trash_msg) = hub
+                .users()
+                .messages_trash("me", &msg_id)
+                .doit()
+                .await
+                .unwrap();
+        }
+        else{
+            println!("{} is a nonexistent message id", msg_id);
+            continue;
+        }
+    }
 }
 
 /// Gmail management program that provides options in interacting with your gmail
@@ -240,8 +287,6 @@ struct Args {
     #[command(subcommand)]
     cmds: Commands,
 }
-
-
 
 #[derive(Parser, Debug, Clone, PartialEq)]
 enum Commands {
@@ -274,50 +319,38 @@ enum Commands {
 
 #[tokio::main]
 async fn main() {
-    
-    // println!("{:?}", list_labels().await);
-
-    // let args: Vec<String> = env::args().map(|x| x.to_string()).collect();
-    // let ref program = args[0];
-
-    // let mut opts = Options::new();
-    // opts.optflag("h", "help", "Display help message.");
-    // opts.optopt("d", "delete", "Delete emails given an id", "[-l] <Label Name | Message ID>");
-    // opts.optflag("l", "labels", "List all labels within the authenticated email");
-
-    // let matches = match opts.parse(&args[1..]){
-    //     Ok(m) => { m }
-    //     Err(f) => {
-    //         println!("{}", f);
-    //         process::exit(1);
-    //     }
-    // };
-
-    // if matches.opt_present("h"){
-    //     print_usage(&program, opts);
-    //     return;
-    // }
-    // if matches.opt_present("d"){
-    //     // let flag_or_id = matches.opt_str("d");
-    //     // println!("{:?}", flag_or_id.clone().unwrap());
-    //     // println!("{:?}", matches.opt_str("d").clone().unwrap());
-    //     println!("{:?}", args);
-    //     // println!("Argument {:?}, Position {:?}", matches.opt_strs("d"), matches.free);
-    //     // match flag_or_id.clone().unwrap().as_str(){
-    //     //     "l" => {trash_messages_from_label(matches.free.join(" ")).await},
-    //     //     _ => println!("It's something else!")
-    //     // };
-    // }
-    // if matches.opt_present("l"){
-
-    // }
-
+    let hub = create_client().await;
     let args = Args::parse();
-    println!("Args: {args:?}");
+    // println!("Args: {args:?}");
 
-    // match args.cmds {
-    //     Commands::Trash
-    // }
+    match args.cmds {
+        Commands::Trash { 
+            labels, msgs
+        } => {
+            if let Some(labels) = labels {
+                trash_messages_from_labels(&hub, labels).await;
+            }
+            if let Some(msgs) = msgs {
+                trash_messages_from_id(&hub, msgs).await;
+            }
+        },
+        Commands::Send { to, subject, description } => todo!(),
+        Commands::Labels => {
+            let labels_hashmap = list_labels(&hub).await;
+            let size = labels_hashmap.len();
+            let mut count = 0;
+            print!("All Labels in authenticated user's inbox: ");
+            for label_id_pair in labels_hashmap{
+                count += 1;
+                if count != size {
+                    print!("{}, ", label_id_pair.0);
+                }
+                else {
+                    print!("{}", label_id_pair.0);
+                }
+            }
+        },
+    }
     
     // if trash != None{
     //     let trash = trash.unwrap();
