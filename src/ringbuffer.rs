d07ef85b939560e25b3cc3facc763f6d5b600ad1
Mahdi Ali-Raihan <asder8215@gmail.com>
2024-08-18 22:56:18 -0400

    Created a generic multithreaded only ring buffer; will likely put this in a separate repo alongside a single threaded version

---
+++ src/ringbuffer.rs
@@ -0,0 +1,119 @@
+use std::fmt::Debug;
+use std::sync::{Condvar, Mutex};
+
+// Arc documentation for threading with mutex and condvar here:
+// Mutex: https://doc.rust-lang.org/stable/std/sync/struct.Mutex.html
+// Condvar: https://doc.rust-lang.org/stable/std/sync/struct.Condvar.html
+/// A ring (circular) buffer struct that can only be used in a multi-threaded environment
+pub struct MultiThreadedRingBuffer<T, const CAPACITY: usize> {
+    num_jobs: (Mutex<usize>, Condvar),
+    inner_rb: Mutex<InnerRingBuffer<T, CAPACITY>>, // state: Arc<(Mutex<State>, Condvar)>
+}
+
+// An inner ring buffer to contain the items, enqueue, and dequeue index for MultiThreadedRingBuffer struct
+struct InnerRingBuffer<T, const CAPACITY: usize> {
+    items: [Option<T>; CAPACITY],
+    enqueue_index: usize,
+    dequeue_index: usize,
+}
+
+/// Implements the InnerRingBuffer functions
+impl<T: Debug, const CAPACITY: usize> InnerRingBuffer<T, CAPACITY> {
+    /// Instantiates the InnerRingBuffer
+    const fn new() -> Self {
+        InnerRingBuffer {
+            items: [const { None }; CAPACITY],
+            enqueue_index: 0,
+            dequeue_index: 0,
+        }
+    }
+}
+
+/// Implements the MultiThreadedRingBuffer functions
+impl<T: Debug, const CAPACITY: usize> MultiThreadedRingBuffer<T, CAPACITY> {
+    /// Instantiates the MultiThreadedRingBuffer
+    pub const fn new() -> Self {
+        MultiThreadedRingBuffer {
+            // How to initialize a generic array of options with None (without needing to iterate hence making it O(1) init)
+            // https://stackoverflow.com/questions/28656387/initialize-a-large-fixed-size-array-with-non-copy-types
+            num_jobs: (Mutex::new(0), Condvar::new()),
+            inner_rb: Mutex::new(InnerRingBuffer::new()),
+        }
+    }
+
+    /// Helper function to add an Option item to the MultiThreadedRingBuffer
+    async fn enqueue_item(&self, item: Option<T>) {
+        let (num_jobs, cvar) = &self.num_jobs;
+        let mut num_jobs = num_jobs.lock().unwrap();
+
+        while *num_jobs == CAPACITY {
+            num_jobs = cvar.wait(num_jobs).unwrap();
+        }
+
+        let mut inner = self.inner_rb.lock().unwrap();
+        let enqueue_index = inner.enqueue_index;
+        inner.items[enqueue_index] = item;
+        *num_jobs += 1;
+        inner.enqueue_index = (inner.enqueue_index + 1) % CAPACITY;
+
+        cvar.notify_one();
+    }
+
+    /// Adds an item of type T to the MultiThreadedRingBuffer so long as there is space in the buffer
+    pub async fn enqueue(&self, item: T) {
+        self.enqueue_item(Some(item)).await;
+    }
+
+    /// Retrieves an item of type T from the MultiThreadedRingBuffer if an item exists in the buffer
+    pub async fn dequeue(&self) -> Option<T> {
+        let (num_jobs, cvar) = &self.num_jobs;
+        let mut num_jobs = num_jobs.lock().unwrap();
+
+        while *num_jobs == 0 {
+            num_jobs = cvar.wait(num_jobs).unwrap();
+        }
+
+        let mut inner = self.inner_rb.lock().unwrap();
+        let dequeue_index = inner.dequeue_index;
+        let item = inner.items[dequeue_index].take();
+        *num_jobs -= 1;
+        inner.dequeue_index = (inner.dequeue_index + 1) % CAPACITY;
+
+        cvar.notify_one();
+
+        item
+    }
+
+    /// Poisons the MultiThreadedRingBuffer with None values up to the capacity of the buffer
+    pub async fn poison(&self) {
+        for _ in 0..CAPACITY {
+            self.enqueue_item(None).await;
+        }
+    }
+
+    /// If the MultiThreadedRingBuffer is poisoned via the poison()
+    /// call or is at capacity, this method will allow the ring buffer
+    /// to be used again and resets it to an empty state
+    pub async fn clear_poison(&self) {
+        let mut num_jobs = self.num_jobs.0.lock().unwrap();
+        if *num_jobs == CAPACITY {
+            *self.inner_rb.lock().unwrap() = InnerRingBuffer::new();
+            *num_jobs = 0;
+        } else {
+            println!("Ring buffer is not poisoned or it is empty");
+        }
+    }
+
+    /// Clears the MultiThreadedRingBuffer back to an empty state
+    pub async fn clear(&self) {
+        let mut num_jobs = self.num_jobs.0.lock().unwrap();
+        *num_jobs = 0;
+        *self.inner_rb.lock().unwrap() = InnerRingBuffer::new();
+    }
+}
+
+impl<T: Debug, const CAPACITY: usize> Default for MultiThreadedRingBuffer<T, CAPACITY> {
+    fn default() -> Self {
+        Self::new()
+     }
+}
